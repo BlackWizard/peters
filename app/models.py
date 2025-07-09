f8d4dc87b9774a1bfec4cbc8c2f07117382e8f6f
Arapov Denis <d.arapov@foxhound.su>
2025-07-09 21:43:07 +0300

    Sync

--- app/models.py
+++ app/models.py
@@ -1,15 +1,7 @@
 from datetime import datetime
-from typing import Annotated
 
-from sqlalchemy import ForeignKey, func, String
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import (
-    DeclarativeBase,
-    Mapped,
-    declared_attr,
-    mapped_column,
-    relationship,
-)
+from sqlalchemy import func, Column
+from sqlalchemy.orm import declared_attr
 
 from fastapi_storages import FileSystemStorage
 from fastapi_storages.integrations.sqlalchemy import FileType, ImageType
@@ -18,20 +10,11 @@ from fastapi_storages.integrations.sqlalchemy import FileType, ImageType
 from sqlmodel import SQLModel, Field, Relationship
 
 
-
 storage = FileSystemStorage(path="/nfs/dvr/plates")
 
-
-int_pk = Annotated[int, mapped_column(primary_key=True)]
-created_at = Annotated[datetime, mapped_column(server_default=func.now())]
-updated_at = Annotated[
-    datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)
-]
-str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
-str_null_true = Annotated[str, mapped_column(nullable=True)]
-
-
 class DBModel(SQLModel):
+    __abstract__ = True
+
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
@@ -53,24 +36,15 @@ class BaseModel(DBModel):
     )
 
 
-class Base(AsyncAttrs, DeclarativeBase):
-    __abstract__ = True
-
-    @declared_attr.directive
-    def __tablename__(cls) -> str:
-        return f"{cls.__name__.lower()}s"
-
-    id: Mapped[int_pk]
-    created_at: Mapped[created_at]
-    updated_at: Mapped[updated_at]
+class AuthorBase(SQLModel):
+    name: str = Field(index=True)
+    short: str | None = Field(index=True, nullable=True)
 
 
-class Author(Base):
-    name: Mapped[str]
-    short: Mapped[str_null_true]
-
-    titles = relationship("Title")
-    # titles = relationship('User', secondary='project_users', back_populates='projects')
+class Author(BaseModel, AuthorBase, table=True):
+    titles: list["Title"] = Relationship(
+        back_populates="author", sa_relationship_kwargs={"lazy": "selectin"}
+    )
 
     def __str__(self):
         return (
@@ -79,18 +53,24 @@ class Author(Base):
             f"name={self.name!r})"
         )
 
+class TitleBase(SQLModel):
+    name: str = Field(index=True)
+    code: str = Field(index=True)
+    year: int | None = None
+    pages: int | None = None
+    author_id: int = Field(foreign_key="authors.id")
+    logo: ImageType = Field(default=None, sa_column=Column(ImageType(storage=storage)))
 
-class Title(Base):
-    name: Mapped[str]
-    code: Mapped[str_null_true]
-    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"), nullable=False)
-    year: Mapped[int] = mapped_column(nullable=True)
-    pages: Mapped[int] = mapped_column(nullable=True)
-    logo = mapped_column(ImageType(storage=storage))
-
-    author: Mapped["Author"] = relationship("Author", back_populates="titles")
-    plates = relationship("TitlePlate")
-    files = relationship("File")
+class Title(BaseModel, TitleBase, table=True):
+    author: Author = Relationship(
+        back_populates="titles", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+    files: list["File"] = Relationship(  # noqa: F821
+        back_populates="title", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+    plates: list["TitlePlate"] = Relationship(  # noqa: F821
+        back_populates="title", sa_relationship_kwargs={"lazy": "selectin"}
+    )
 
     def __str__(self):
         return (
@@ -100,12 +80,15 @@ class Title(Base):
         )
 
 
-class TitlePlate(Base):
-    title_id: Mapped[int] = mapped_column(ForeignKey("titles.id"), nullable=False)
-    plate: Mapped[int]
-    position: Mapped[int]
+class  TitlePlateBase(SQLModel):
+    title_id: int = Field(foreign_key="titles.id", nullable=False)
+    plate: int
+    position: int = Field(default=1)
 
-    title: Mapped["Title"] = relationship("Title", back_populates="plates")
+class TitlePlate(BaseModel, TitlePlateBase, table=True):
+    title: Title = Relationship(
+        back_populates="plates", sa_relationship_kwargs={"lazy": "selectin"}
+    )
 
     def __str__(self):
         return (
@@ -132,30 +115,19 @@ class Source(BaseModel, SourceBase, table=True):
             f"url={self.url!r})"
         )
 
+class FileBase(SQLModel):
+    title_id: int = Field(foreign_key="titles.id")
+    source_id: int = Field(foreign_key="sources.id")
+    file: FileType = Field(sa_column=Column(FileType(storage=storage)))
+    url: str | None = None
 
-"""
-class Source(Base):
-    name: Mapped[str]
-    url: Mapped[str]
-
-    files = relationship("File")
-
-    def __str__(self):
-        return (
-            f"{self.__class__.__name__}(id={self.id}, "
-            f"name={self.name!r}, "
-            f"url={self.url!r})"
-        )
-"""
-
-
-class File(Base):
-    title_id: Mapped[int] = mapped_column(ForeignKey("titles.id"), nullable=False)
-    title: Mapped["Title"] = relationship("Title", back_populates="files")
-    source_id: Mapped[int] = mapped_column(ForeignKey("sources.id"), nullable=False)
-    source: Mapped["Source"] = relationship("Source", back_populates="files")
-    file = mapped_column(FileType(storage=storage))
-    url: Mapped[str_null_true]
+class File(BaseModel, FileBase, table=True):
+    title: Title = Relationship(
+        back_populates="files", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+    source: Source = Relationship(
+        back_populates="files", sa_relationship_kwargs={"lazy": "selectin"}
+    )
 
     def __str__(self):
         return (
